ed7946323e6b8a4bda02e2ed5f6e81a807d4fbf9
kirknasty54 <kirkpatrick545454@gmail.com>
2025-11-03 12:09:47 -0500

    first commit and setting up navbar and footer

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
+
+function App() {
+  return (
+      <div>
+          <Navbar/>
+          <Footer/>
+      </div>
+  )
+}
+
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+export default function Footer(){
+    return(
+        <footer className={"bg-white rounded-lg shadow-sm m-4 dark:bg-gray-800"}>
+            <div className={"w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between"}>
+                      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="https://github.com/kirknasty54" class="hover:underline">Kirknasty Industries</a>. All Rights Reserved.
+                      </span>
+                <ul className={"flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0"}>
+                    <li>
+                        <a href={"https://github.com/kirknasty54"} className={"me-4 hover:underline md:me-6"}>Github</a>
+                    </li>
+                    <li>
+                        <a href={"https://www.linkedin.com/in/jackson-kirkpatrick-8b4018224/"} className={"me-4 hover:underline md:me-6"}>LinkedIn</a>
+                    </li>
+                    <li>
+                        <a href={"mailto:kirkpatrick545454@gmail.com?subject=Book Request&body=I LOVE BOOKS. INJECT BOOKS INTO MY BLOOD STREAM!"} className={"me-4 hover:underline md:me-6"}>Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+    )
+}
